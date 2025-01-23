5c8f2974a91eb3b53916103e6363b087ab82dfe2
dominik9 <dominik3gumis@gmail.com>
2025-01-23 21:07:01 +0100

    config cmake

--- tests/pizzas_tests/PizzeriaTest.cpp
+++ tests/pizzas_tests/PizzeriaTest.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <string>
-#include "mocks/PizzaMock.hpp"
+#include "../mocks/PizzaMock.hpp"
 #include "Pizzeria.hpp"
 #include "Margherita.hpp"
 #include "Funghi.hpp"
